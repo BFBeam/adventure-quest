412e4911e3897114b3fb471ea28cc2239e2e4351
Austin Beam <austindbeam@gmail.com>
2020-07-29 16:05:01 -0400

    Played with Story button JS.

--- JavaScript/story.js
+++ JavaScript/story.js
@@ -1,20 +1,9 @@
-// Adds functionality to the buttons in the storyArc folder.
-
-document.getElementById("firstLink").onclick = function() {
-    location.href = "story1.html"
-};
-document.getElementById("secondLink").onclick = function() {
-    location.href = "story2.html" 
-}; 
-document.getElementById("thirdLink").onclick = function() {
-    location.href = "story3.html" 
-}; 
-document.getElementById("lastLink").onclick = function() {
-    location.href = "story.html" 
-};
-
 // Gives user a message based on their page in the story.
 
 document.getElementById("firstAlert").onclick = function() {
     alert("Twaine hid in their wardrobe but accidently got locked in. Please try again.")
+};
+
+document.getElementById("firstAlert").onclick = function() {
+    alert("You begin yelling at the adventurers for waking you up but they ignore your complaints until you give up and wonder back inside.")
 };
